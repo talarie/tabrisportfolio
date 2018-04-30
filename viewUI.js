1904385e0f2ec1831127e3aed2ca20637134bbc2
talarie <31778957+talarie@users.noreply.github.com>
2018-04-30 19:08:45 -0400

    change background and status bar colour

--- viewUI.js
+++ viewUI.js
@@ -1,12 +1,13 @@
 const {ImageView, ui} = require('tabris');
 
 createImageView('fit');
+ui.statusBar.background = '#dbc73b'
+ui.contentView.background = '#000';
 
 function createImageView(scaleMode) {
   new ImageView({
     left: 10, top: 'prev() 10', width: 350, height: 500,
     image: 'https://github.com/talarie/tabrisportfolio/blob/master/spaceUI.png?raw=true',
-    background: '#aaaaaa',
     scaleMode: scaleMode
   }).appendTo(ui.contentView);
 }
