4efd1c265be207d0ba4001d8a74ea36504edb823
talarie <31778957+talarie@users.noreply.github.com>
2018-04-30 20:47:56 -0400

    add button to ui

--- viewUI.js
+++ viewUI.js
@@ -1,4 +1,8 @@
-const {ImageView, ui} = require('tabris');
+
+const {ImageView, TextView, Button, ui, app} = require('tabris');
+
+app.registerFont('Futura', 'https://github.com/talarie/tabrisportfolio/blob/master/Futura.ttc?raw=true');
+let font = '20px Futura';
 
 createImageView('fit');
 ui.statusBar.background = '#dbc73b'
@@ -6,8 +10,19 @@ ui.contentView.background = '#000';
 
 function createImageView(scaleMode) {
   new ImageView({
-    left: 10, top: 'prev() 10', width: 350, height: 500,
+    top: 'prev() 10', width: 350, height: 500, centerX: 0,
     image: 'https://github.com/talarie/tabrisportfolio/blob/master/spaceUI.png?raw=true',
     scaleMode: scaleMode
   }).appendTo(ui.contentView);
+
+let btnEnter = new Button({
+      class: 'main',
+      centerX: 0,
+      top: 'prev() 10',
+      text: 'Enter Details',
+      font: font,
+    })
+  .appendTo(ui.contentView);
+
+  ui.find('.main').set('textColor', 'black');
 }
